e1c88983cb9bfb63d7b6e5b70eae3a447af65fc4
nschutz <nschutz@student.42wolfsburg.de>
2024-03-26 09:55:38 +0100

    CPP07 feat ex01: to stupid to write include correct, finished ex01

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -1,9 +1,10 @@
 #pragma once
 
 #include <iterator>
+#include <iostream>
 
 template <typename T>
-T iter(T *array, size_t length, void (*func)(T const &))
+void iter(T *array, size_t length, void (*func)(T &))
 {
     for (size_t i = 0; i < length; i++)
         func(array[i]);
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,8 +1,12 @@
 #include "iter.hpp"
 
+void print(int &i) {
+    std::cout << i << std::endl;
+}
+
 int main()
 {
     int tab[] = { 0, 1, 2, 3, 4 };
-    ::iter(tab, 5, printf());
+    iter(tab, 5, print);
     return 0;
 }
