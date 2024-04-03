d8db8a7ea52d69edf0fb2d422faf8e0b506ab210
nschutz <nschutz@student.42wolfsburg.de>
2024-04-03 11:37:49 +0200

    CPP07 feat ex01: more test cases

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,12 +1,18 @@
 #include "iter.hpp"
 
-void print(int &i) {
+void printi(int &i) {
     std::cout << i << std::endl;
+}
+
+void prints(std::string &str) {
+    std::cout << str << std::endl;
 }
 
 int main()
 {
     int tab[] = { 0, 1, 2, 3, 4 };
-    iter(tab, 5, print);
+    iter(tab, 6, printi);
+    std::string tab2[] = { "Hello", "World", "!" };
+    iter(tab2, 3, prints);
     return 0;
 }
