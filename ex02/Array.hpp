243a3350b0eff680353045bdf6da5f8f11dadbf9
nschutz <nschutz@student.42wolfsburg.de>
2024-04-03 11:10:21 +0200

    CPP07 feat ex02: making it more visible

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -10,24 +10,32 @@ class Array
     public:
         Array() : _array(new T[0]), _size(0) {}
         Array(unsigned int n) : _array(new T[n]), _size(n) {}
-        Array(Array<T> const &src) : _array(new T[src._size]), _size(src._size) {
+
+        Array(Array<T> const &src) : _array(new T[src._size]), _size(src._size)
+        {
             for (unsigned int i = 0; i < this->_size; i++)
                 this->_array[i] = src._array[i];
         }
-        Array<T> &operator=(const Array<T> &rhs){
-        _array = (new T[rhs._size]);
-        _size = (rhs._size);
-        for (unsigned int i = 0; i < this->_size; i++)
+
+        Array<T> &operator=(const Array<T> &rhs)
+        {
+            _array = (new T[rhs._size]);
+            _size = (rhs._size);
+            for (unsigned int i = 0; i < this->_size; i++)
                 this->_array[i] = rhs._array[i];
             return *this;
         }
-        ~Array() {
+
+        ~Array()
+        {
             delete[] this->_array;
         }
 
+
         unsigned int size(){ return this->_size; }
 
-        T& operator[](unsigned int i){
+        T& operator[](unsigned int i)
+        {
             if (i >= this->_size)
                 throw std::exception();
             return this->_array[i];
